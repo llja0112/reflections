cdaf62ad7d054bc3a92147abedb6899a65bba6a1
Anthony Li <llja0112@gmail.com>
2017-10-01 16:19:12 +0800

    Add tokenizer feature to typeahead

--- app/assets/javascripts/reflections.js
+++ app/assets/javascripts/reflections.js
@@ -4,7 +4,7 @@ Reflections.reflections = Reflections.reflections || {};
 Reflections.reflections.show = {
   init: function() {
     var names = new Bloodhound({
-      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('user_details'),
+      datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
       queryTokenizer: Bloodhound.tokenizers.whitespace,
       remote: {
         url: '/users/typeahead/%QUERY',
@@ -12,11 +12,20 @@ Reflections.reflections.show = {
       }
     });
 
-    $('#review_input').typeahead({
-      minLength: 3
-    }, {
-      displayKey: 'user_details',
-      source: names.ttAdapter()
+    // $('#review_input').typeahead({
+    //   minLength: 3
+    // }, {
+    //   displayKey: 'user_details',
+    //   source: names.ttAdapter()
+    // });
+
+    $('#review_input').tokenfield({
+      typeahead: [{
+        minLength: 3
+      }, {
+        displayKey: 'value',
+        source: names.ttAdapter()
+      }]
     });
   }
 }
