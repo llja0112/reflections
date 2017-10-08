36b5d8f0b44062969c50f30fe4301ab653f11091
Anthony Li <llja0112@gmail.com>
2017-10-08 14:47:07 +0800

    Fix summernotes turbolinks bug

--- app/assets/javascripts/shared.js
+++ app/assets/javascripts/shared.js
@@ -1,11 +1,14 @@
 var Reflections = Reflections || {};
 Reflections.init = function() {
-  $('.summernote').each(function(){
-    $(this).summernote({
-      height: 500,                 // set editor height
-      minHeight: null,             // set minimum height of editor
-      maxHeight: null,             // set maximum height of editor
-      focus: true                  // set focus to editable area after initializing summernote
-    });
+  $(document).on('turbolinks:load', function() {
+    $('.summernote').each(function() {
+      if ($(this).is(":visible"))
+        $(this).summernote({
+          height: 500,                 // set editor height
+          minHeight: null,             // set minimum height of editor
+          maxHeight: null,             // set maximum height of editor
+          focus: true                  // set focus to editable area after initializing summernote
+        });
+    })
   });
 };
