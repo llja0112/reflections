41ed1b6044dbc5537abe222ce0afd1ede2de0e6d
Anthony Li <llja0112@gmail.com>
2017-10-08 11:43:31 +0800

    Add review controller and allow user to add reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,30 @@
 class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+
   def show
-    @review = Review.find(review_id_param)
+    @review = Review.find(review_id_params)
   end
 
   def edit
+    @review = Review.find(review_id_params)
   end
 
   def update
+    review = Review.find(review_id_params)
+    review.content = review_content_params
+    review.save
+
+    redirect_to :back
   end
 
   private
 
-  def review_id_param
+  def review_id_params
     params.require(:id)
+  end
+
+  def review_content_params
+    params.require(:review).permit(
+      :content)['content']
   end
 end
