60e0c2924c31d3c09ce299b6c8f04b7dc3631dfb
Anthony Li <llja0112@gmail.com>
2017-10-09 18:03:14 +0800

    Change review cards to standardised reflection cards

--- app/helpers/reflections_helper.rb
+++ app/helpers/reflections_helper.rb
@@ -2,14 +2,29 @@ module ReflectionsHelper
   def reflection_card_class(reflection)
     if user_signed_in?
       if reflection.user == current_user
-        class_string = 'border-primary'
+        class_string = 'primary'
       elsif reflection.reviewers.include?(current_user)
-        class_string = 'border-warning'
+        class_string = 'warning'
       else
-        class_string = 'border-info'
+        class_string = 'info'
       end
-      return class_string
+    else
+      class_string = 'info'
     end
+    return class_string
+  end
+
+  def reflection_card_description(reflection)
+    if reflection.user == current_user
+      description_string = 'Personal reflection'
+    elsif reflection.reviewers.include?(current_user)
+      description_string = 'For your review'
+    elsif reflection.privacy == 'Private'
+      description_string = 'Shared with you'
+    else
+      description_string = 'Public reflection'
+    end
+    return description_string
   end
 
   def privacy_share_form(privacy)
