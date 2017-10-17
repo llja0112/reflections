bd0d4b96da2a2e5ee15c2a0bf40a53f4ef8a8204
Anthony Li <llja0112@gmail.com>
2017-10-17 11:33:39 +0800

    Change mailer name and titles of emails

--- app/mailers/custom_mailer.rb
+++ app/mailers/custom_mailer.rb
@@ -1,4 +1,5 @@
 class CustomMailer < Devise::Mailer
   default message_id: "<#{SecureRandom.uuid}@mg.medbot.me>"
+  default from: "Reflections Admin <postmaster@mg.medbot.me>"
   layout 'mailer'
 end
